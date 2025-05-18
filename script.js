705c73c76c8f991b363ed208597ac9750d384436
tamirlan-bit <ktimor@gmail.com>
2025-05-17 22:54:20 -0700

    rewriting from scratch, CSS and HTML done, display works for single digits and ops

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
 console.log("JS works");
 
+const display = document.querySelector('.display');
+const numberBtns = document.querySelectorAll('.number');
+const operatorBtns = document.querySelectorAll('.operator');
+const equalBtn = document.querySelector('.equal');
+const clearBtn = document.querySelector('.clear');
 
+let lastPressed = '';
+let storedValue = 0;
+let appendFlag = false;
 
+// Update the display
+function updateDisplay(value) {
+    display.textContent = value;
+  }
 
+// Numbers event listeners
+numberBtns.forEach( number=> {
+    number.addEventListener('click', () => {
+        lastPressed = Number(number.textContent);
+        storedValue += lastPressed;
+        updateDisplay(storedValue);
+        console.log(lastPressed);
+    }
+)});
 
+// Operator event listeners
+operatorBtns.forEach( operator => {
+    operator.addEventListener('click', () => {
+        display.append(operator.textContent);
+        console.log(`Opertaor ${operator.textContent} was Pressed`);
+    }
+)});
 
+// Equal event listeners
+equalBtn.addEventListener('click', () => {
+    console.log(`Equal is no joke`);
+});
+
+// Clear event listener
+clearBtn.addEventListener('click', () => {
+    updateDisplay('CLEARED');
+    setTimeout(() => { updateDisplay(0); },700);
+    console.log(`Time to cleanup display`);
+});
